44647d45db600ada296ea1068d6cb5c164da930d
bryanb213 <blancasbryan@gmail.com>
2020-03-11 17:43:08 -0500

    added heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors')
-
+const path = require('path')
 const app = express();
 
 app.use(cors());
@@ -16,6 +16,14 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+//serve
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
