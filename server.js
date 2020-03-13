427f04f9a9a37c01d229fb0743bc1be12c6a6243
bryanb213 <blancasbryan@gmail.com>
2020-03-13 15:10:55 -0500

    fixed favicon

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const path = require('path')
 const app = express();
 
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 //conect DB
 connectDB();
@@ -25,6 +26,6 @@ if(process.env.NODE_ENV === 'production') {
     });
 }
 
-const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
