558e929f4444ed678bc6680b3f73222ce15629e7
bryanb213 <blancasbryan@gmail.com>
2020-03-17 14:08:13 -0500

    added form group

--- client/src/components/Routing/PrivateRoute.jsx
+++ client/src/components/Routing/PrivateRoute.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
 import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const PrivateRoute = ({component: Component, auth:{ isAuthenticated, loading } ,...rest }) => (
-    <Route 
-        {...rest} 
-        render={props => 
-        !isAuthenticated && !loading ? (
-            <Redirect to='/login' />
-        ) : (
-            <Component {...props} />
-        )} />
-)
+const PrivateRoute = ({ component: Component, auth, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            auth.isAuthenticated === true ? (
+                <Component {...props} />
+            ) : (
+                    <Redirect to="/login" />
+                )
+        }
+    />
+);
+
 PrivateRoute.propTypes = {
-    auth: PropTypes.object.isRequired,
-}
+    auth: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
     auth: state.auth
-})
+});
 
 export default connect(mapStateToProps)(PrivateRoute);
