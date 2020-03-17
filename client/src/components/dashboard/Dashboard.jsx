e045663b200fda4ec5b4f99b08b6c8966414efbb
bryanb213 <blancasbryan@gmail.com>
2020-03-17 14:45:48 -0500

    added credentials

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,55 +1,91 @@
-import React, { useEffect, Fragment } from 'react';
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { getCurrentProfile, deleteAccount } from '../../actions/profile';
-import Spinner from '../layout/spinner'
-import {  Link } from 'react-router-dom';
-import DashboardActions from './DashboardActions.jsx';
+import Spinner from '../common/Spinner';
+import DashboardActions from './DashboardActions';
 import Experience from './Experience';
-import Education from './Education'
+import Education from './Education';
 
+class Dashboard extends Component {
+    componentDidMount() {
+        this.props.getCurrentProfile();
+    }
 
+    onDeleteClick(e) {
+        this.props.deleteAccount();
+    }
 
-const Dashboard = ({ deleteAccount, getCurrentProfile, auth: { user }, profile: { loading, profile } }) => {
-    useEffect(() => {
-        getCurrentProfile();
-    }, [getCurrentProfile]);
+    render() {
+        const { user } = this.props.auth;
+        const { profile, loading } = this.props.profile;
 
-    return (
-        loading && profile === null ? <Spinner /> : <Fragment>
-            <h1 className="large text-primary">Dashboard</h1>
-            <p className="lead">
-            <i className="fas fa-user"></i> Welcome { user && user.name }
-            </p>
-            {profile !== null ? <Fragment>
-                <DashboardActions />
-                <Experience experience={profile.experience} />
-                <Education education={profile.education} />
-                <div className="my-2">
-                    <button className="btn btn-danger">
-                        <i className="fas fas-user-minus"></i>
-                         Delete my account
-                    </button>
+        let dashboardContent;
+
+        if (profile === null || loading) {
+            dashboardContent = <Spinner />;
+        } else {
+            // Check if logged in user has profile data
+            if (Object.keys(profile).length > 0) {
+                dashboardContent = (
+                    <div>
+                        <p className="lead text-muted">
+                            Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
+                        </p>
+                        <DashboardActions />
+                        <Experience experience={profile.experience} />
+                        <Education education={profile.education} />
+                        <div style={{ marginBottom: '60px' }} />
+                        <button
+                            onClick={this.onDeleteClick.bind(this)}
+                            className="btn btn-danger"
+                        >
+                            Delete My Account
+            </button>
+                    </div>
+                );
+            } else {
+                // User is logged in but has no profile
+                dashboardContent = (
+                    <div>
+                        <p className="lead text-muted">Welcome {user.name}</p>
+                        <p>You have not yet setup a profile, please add some info</p>
+                        <Link to="/create-profile" className="btn btn-lg btn-info">
+                            Create Profile
+            </Link>
+                    </div>
+                );
+            }
+        }
+
+        return (
+            <div className="dashboard">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4">Dashboard</h1>
+                            {dashboardContent}
+                        </div>
+                    </div>
                 </div>
-            </Fragment> : 
-            <Fragment>
-                <p>You have not set up a profile yet, please add some info</p>
-                <Link to='create-profile' className="btn btn-primary my-1" >Create Profile</Link>
-            </Fragment>}
-        </Fragment>
-    )
-};
+            </div>
+        );
+    }
+}
 
 Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
+    deleteAccount: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
-    profile: PropTypes.object.isRequired,
-    deleteAccount: PropTypes.func.isRequired
-}
+    profile: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    profile: state.profile
-})
+    profile: state.profile,
+    auth: state.auth
+});
 
-export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+    Dashboard
+);
--- client/src/components/dashboard/DashboardActions.jsx
+++ client/src/components/dashboard/DashboardActions.jsx
@@ -4,16 +4,18 @@ import { Link } from 'react-router-dom'
 
 const DashboardActions = () => {
     return (
-        <div className="dash-buttons">
-            <Link to="/edit-profile" class="btn btn-light"
-            ><i className="fas fa-user-circle text-primary"></i> Edit Profile</Link
-            >
-            <Link to="/add-experience" class="btn btn-light"
-            ><i className="fab fa-black-tie text-primary"></i> Add Experience</Link
-            >
-            <Link to="/add-education" class="btn btn-light"
-            ><i className="fas fa-graduation-cap text-primary"></i> Add Education</Link
-            >
+        <div className="btn-group mb-4" role="group">
+            <Link to="/edit-profile" className="btn btn-light">
+                <i className="fas fa-user-circle text-info mr-1" /> Edit Profile
+      </Link>
+            <Link to="/add-experience" className="btn btn-light">
+                <i className="fab fa-black-tie text-info mr-1" />
+        Add Experience
+      </Link>
+            <Link to="/add-education" className="btn btn-light">
+                <i className="fas fa-graduation-cap text-info mr-1" />
+        Add Education
+      </Link>
         </div>
     );
 }
