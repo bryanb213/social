44313ac14de11e1c477d905b162dfb14c16eb2dd
bryanb213 <blancasbryan@gmail.com>
2020-03-18 17:10:09 -0500

    fixed api call

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('user');
-const keys = require('../config/keys_dev');
+const keys = require('../config/keys_dev').secretOrKey;
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
